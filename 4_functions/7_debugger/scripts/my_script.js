572c26062cea40f8a4d93f8482a6520db4f5915a
Josh Archer <jarcher@greenriver.edu>
2021-02-01 09:37:35 -0800

    Updated the primes debugger example to remove several typos.

--- 4_functions/7_debugger/scripts/my_script.js
+++ 4_functions/7_debugger/scripts/my_script.js
@@ -2,9 +2,11 @@
 let primeList = document.getElementById("prime-list");
 let first = document.getElementById("first-check");
 let second = document.getElementById("second-check");
+let third = document.getElementById("third-check");
 
-first.innerHTML = isPrime(299);
-second.innerHTML = isPrime(191);
+first.innerHTML = isPrime(7);
+second.innerHTML = isPrime(299);
+third.innerHTML = isPrime(191);
 
 for (let i = 1; i <= 300; i++)
 {
