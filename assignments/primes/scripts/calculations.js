9e16e74a8f57b854f13f273109e640e8ac0eea3b
Josh Archer <jarcher@greenriver.edu>
2021-02-10 22:56:28 -0800

    Adding the starter files for a project on prime numbers.

---
+++ assignments/primes/scripts/calculations.js
@@ -0,0 +1,17 @@
+//register button clicks when the page loads
+window.onload = function() {
+    let analyze = document.getElementById("analyze");
+    let generate = document.getElementById("generate");
+    analyze.onclick = analyzeNumberOnClick;
+    generate.onclick = generatePrimesOnClick;
+}
+
+function analyzeNumberOnClick(event)
+{
+
+}
+
+function generatePrimesOnClick(event)
+{
+
+}
